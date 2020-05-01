41cc073889ccd2426cd5e487512d64bbfe9b73bb
Ilya <ilyailyukou@gmail.com>
2020-05-01 23:54:00 +0300

    Updates

---
+++ precache-manifest.ff5f49868cca22a491266e816fec8673.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc3b166ef0b59b40107997ff9be5491d",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "bdd24b4d24581396e2d1",
+    "url": "/react-gh-pages/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "54f7ca2a7771496a2ad7",
+    "url": "/react-gh-pages/static/css/main.4a5d5d3d.chunk.css"
+  },
+  {
+    "revision": "bdd24b4d24581396e2d1",
+    "url": "/react-gh-pages/static/js/2.cc0ba37b.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/react-gh-pages/static/js/2.cc0ba37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54f7ca2a7771496a2ad7",
+    "url": "/react-gh-pages/static/js/main.7806a0df.chunk.js"
+  },
+  {
+    "revision": "794dcf058e2038c79182",
+    "url": "/react-gh-pages/static/js/runtime-main.599515ac.js"
+  }
+]);
