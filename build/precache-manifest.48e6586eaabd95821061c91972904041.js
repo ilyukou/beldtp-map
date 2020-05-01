0d29e74a0bdc266e6852dc0f8b0b50b85365cc49
Ilya <ilyailyukou@gmail.com>
2020-05-02 00:17:33 +0300

    Deploying a React App (created using create-react-app) to GitHub Pages

---
+++ build/precache-manifest.48e6586eaabd95821061c91972904041.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbdeaad8e8554e56c6ebb39306b6485a",
+    "url": "/beldtp-map/index.html"
+  },
+  {
+    "revision": "bdd24b4d24581396e2d1",
+    "url": "/beldtp-map/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "f4b7445bf34b498d3b63",
+    "url": "/beldtp-map/static/css/main.4a5d5d3d.chunk.css"
+  },
+  {
+    "revision": "bdd24b4d24581396e2d1",
+    "url": "/beldtp-map/static/js/2.cc0ba37b.chunk.js"
+  },
+  {
+    "revision": "6791e7fb9584d65ed87d655035c7da95",
+    "url": "/beldtp-map/static/js/2.cc0ba37b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4b7445bf34b498d3b63",
+    "url": "/beldtp-map/static/js/main.e152e76e.chunk.js"
+  },
+  {
+    "revision": "dd0acc28c752afb73d96",
+    "url": "/beldtp-map/static/js/runtime-main.9175e875.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.ff5f49868cca22a491266e816fec8673.js"
+  "/beldtp-map/precache-manifest.48e6586eaabd95821061c91972904041.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-gh-pages/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/beldtp-map/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
